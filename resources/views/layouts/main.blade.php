fba4357ee9d1b36ec29fd39446ebc50fbb2ed488
edoaurahman <edoaurahman@gmail.com>
2021-08-21 11:40:50 +0700

    first push

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('template') }}/assets/img/icon.png" rel="icon">
-  <link href="{{ asset('template') }}/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="template/assets/img/icon.png" rel="icon">
+  <link href="template/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('template') }}/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="{{ asset('template') }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="{{ asset('template') }}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="{{ asset('template') }}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="{{ asset('template') }}/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="{{ asset('template') }}/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="template/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="template/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="template/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="template/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="template/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="template/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('template') }}/assets/css/style.css" rel="stylesheet">
+  <link href="template/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Green - v4.3.0
