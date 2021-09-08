888bc7494c124209785e2256dcd78cdc9b33c193
edoaurahman <edoaurahman@gmail.com>
2021-09-09 05:03:44 +0700

    rename title

--- resources/views/admin/content-yt-img.blade.php
+++ resources/views/admin/content-yt-img.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Update Slide</title>
+    <title>Update Yt dan Gambar</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
