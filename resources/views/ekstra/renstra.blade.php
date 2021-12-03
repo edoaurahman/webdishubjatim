49aa5520c04214bbdaac0caed7bd9a0d762e6ede
edoaurahman <edoaurahman@gmail.com>
2021-12-03 16:57:34 +0700

    test

--- resources/views/ekstra/renstra.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-@section('content')
-    <div class="container mt-5 p-5 border">
-        <iframe src="{{ asset('template/assets/ekstra/RENSTRA.pdf') }}" type="application/pdf"
-            class="w-100 mx-auto d-block" height="600">
-        </iframe>
-        <hr>
-    </div>
-@endsection
