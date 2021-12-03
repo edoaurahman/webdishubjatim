48b7599e1710a615535533e716cbbffea2777fd0
edoaurahman <edoaurahman@gmail.com>
2021-12-03 16:44:15 +0700

    done add minimal request

--- resources/views/ekstra/iku-2014-2019.blade.php
+++ resources/views/ekstra/iku-2014-2019.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.main')
 @section('content')
     <div class="container mt-5 p-5 border">
-
-        <embed src="{{ asset('template/assets/ekstra/IKU_2014-2019.pdf') }}" class="w-100 mx-auto d-block" height="600"
-            type="">
+        <iframe src="{{ asset('template/assets/ekstra/IKU_2014-2019.pdf') }}" type="application/pdf"
+            class="w-100 mx-auto d-block" height="600">
+            <p>Your web browser doesn't have a PDF plugin.
+                Instead you can <a href="{{ asset('template/assets/ekstra/IKU_2014-2019.pdf') }}">click here to
+                    download the PDF file.</a></p>
+        </iframe>
         <hr>
     </div>
 @endsection
--- resources/views/ekstra/lkjip2020.blade.php
+++ resources/views/ekstra/lkjip2020.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.main')
 @section('content')
     <div class="container mt-5 p-5 border">
-
-        <embed src="{{ asset('template/assets/ekstra/LKJIP2020.pdf') }}" class="w-100 mx-auto d-block" height="600"
-            type="">
+        <iframe src="{{ asset('template/assets/ekstra/LKJIP2020.pdf') }}" type="application/pdf"
+            class="w-100 mx-auto d-block" height="600">
+            <p>Your web browser doesn't have a PDF plugin.
+                Instead you can <a href="{{ asset('template/assets/ekstra/LKJIP2020.pdf') }}">click here to
+                    download the PDF file.</a></p>
+        </iframe>
         <hr>
     </div>
 @endsection
