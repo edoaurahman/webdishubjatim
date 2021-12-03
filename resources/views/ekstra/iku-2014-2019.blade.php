443a949e1c1c34573ed2af2a74b24500145c5311
edoaurahman <edoaurahman@gmail.com>
2021-12-03 16:45:49 +0700

    done add minimal request

--- resources/views/ekstra/iku-2014-2019.blade.php
+++ resources/views/ekstra/iku-2014-2019.blade.php
@@ -3,9 +3,6 @@
     <div class="container mt-5 p-5 border">
         <iframe src="{{ asset('template/assets/ekstra/IKU_2014-2019.pdf') }}" type="application/pdf"
             class="w-100 mx-auto d-block" height="600">
-            <p>Your web browser doesn't have a PDF plugin.
-                Instead you can <a href="{{ asset('template/assets/ekstra/IKU_2014-2019.pdf') }}">click here to
-                    download the PDF file.</a></p>
         </iframe>
         <hr>
     </div>
