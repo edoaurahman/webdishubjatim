93669c276394176f25299323268843027baa467d
edoaurahman <edoaurahman@gmail.com>
2021-12-04 08:43:50 +0700

    add delete image

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,35 +1,38 @@
 <html>
+
 <head>
-	<title>{{ $title }}</title>
-    <link rel="stylesheet" href="template/assets/css/login.css">
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/css/login.css">
 </head>
+
 <body>
-    <?php 
+    <?php
     session_start();
     
     ?>
     <div class="wrapper fadeInDown">
         <div id="formContent">
-          <!-- Tabs Titles -->
-          <h2 class="active"> Login Admin </h2>
-      <!--     <h2 class="inactive underlineHover">Sign Up </h2> -->
-      
-          <!-- Icon -->
-          <div class="fadeIn first">
-      <!--       <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> -->
-          </div>
-      
-          <!-- Login Form -->
-          <form action="{{ route('postlogin') }}" method="post">
-            {{ csrf_field() }}
-            @csrf
-            <input type="text" id="login" class="fadeIn second" name="username" placeholder="Username">
-            <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password">
-            <input type="submit" class="fadeIn fourth" value="Log In">
-          </form>     
-          {{-- <a href="{{ url('/') }}" class="back">Back</a> --}}
+            <!-- Tabs Titles -->
+            <h2 class="active"> Login Admin </h2>
+            <!--     <h2 class="inactive underlineHover">Sign Up </h2> -->
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <!--       <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> -->
+            </div>
+
+            <!-- Login Form -->
+            <form action="{{ route('postlogin') }}" method="post">
+                {{ csrf_field() }}
+                @csrf
+                <input type="text" id="login" class="fadeIn second" name="username" placeholder="Username">
+                <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password">
+                <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+            {{-- <a href="{{ url('/') }}" class="back">Back</a> --}}
         </div>
-      </div>
+    </div>
 
 </body>
+
 </html>
